93e4dae245e5aa4545d7e0b53503a961f78bd8b4
Mehmet Yıldırım <xmehmetyz@gmail.com>
2022-09-01 14:52:14 +0300

    substring örneği

---
+++ substring.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(){
+
+// dosya windowsta code page olarak windows 1254 olarak kayıt edilmeli!
+  setlocale(LC_ALL, "Turkish");
+
+  int start = 0;
+  int length = 3;
+  char str[100]="Quick brown fox jump over";
+  char substr[100];
+  int i=0;
+  while (i<length)
+  {
+    substr[i]=str[start + i];
+    i++;
+  }
+
+substr[i]='\0';
+  printf("%s", substr);
+  
+
+
+  
+
+  printf("Merhaba, Dünya!");
+
+
+  return 0;
+}
